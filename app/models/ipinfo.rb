7269bf6ab941dfe5a71259b7f549774952b0621d
Alex Quan <alxquan@gmail.com>
2017-02-26 22:14:50 -0700

    started API setup

--- app/models/ipinfo.rb
+++ app/models/ipinfo.rb
@@ -1,2 +1,11 @@
 class Ipinfo < ApplicationRecord
+  require 'curb'
+
+  def other_ip_address(address)
+    Curl.get("www.ipinfo.io/#{address}")
+  end
+
+  def personal_ip_address
+    Curl.get("www.ipinfo.io")
+  end
 end
